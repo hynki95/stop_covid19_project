d599ad35e7f90162da6f07632b548a402bd6bc68
chohyunki <wdlcho@wedatalab.com>
2020-05-11 00:36:18 +0900

    fix a bug and change

--- survey.sol
+++ survey.sol
@@ -10,22 +10,18 @@ contract SurveyContract{
         address Surveyer;
         string purpose;
         string SNS;
+        uint goal;
         uint TotalQuestionsheet;
         uint TotalAnswersheet;
         mapping(uint =>Questionsheet) QuestionsheetIndex;
         mapping(uint =>Answersheet) AnswersheetIndex;
     }
     
-    struct Answersheet{
-        address Answerer;
-        uint TotalInfo;
-        uint TotalAnswer;
-        mapping(uint => Infos) InfoIndex;
-        mapping(uint => Answers) AnswerIndex;
-    }
     
-    struct Infos{
-        string info;
+    struct Answersheet{
+        uint TotalAnswer;
+        address answerer;
+        mapping(uint => Answers) AnswerIndex;
     }
     
     struct Answers{
@@ -41,22 +37,33 @@ contract SurveyContract{
         string question;
     }
     
+    mapping (address => Info) public InfoIndex;
+    
+    struct Info{
+        uint age;
+        string sex;
+        string heigth;
+        string weigth;
+        string country;
+    }
+    
     ///////////////////////////////function///////////////////////////////////////////
     
-    function makeSurvey(string memory _purpose, string memory _SNS) public{
+    function makeSurvey(string memory _purpose, string memory _SNS, uint _goal) public{
         Surveysheet storage _Surveysheet = surveyindex[TotalSurvey];
         require(_Surveysheet.surveying == false);
         _Surveysheet.Surveyer=msg.sender;
         _Surveysheet.surveying = true;
         _Surveysheet.purpose = _purpose;
         _Surveysheet.SNS = _SNS;
+        _Surveysheet.goal = _goal;
     }
     
     function endSurvey() public{
         Surveysheet storage _Surveysheet = surveyindex[TotalSurvey];
         require(_Surveysheet.Surveyer == msg.sender);
         TotalSurvey++;
-        _Surveysheet.surveying == false;
+        surveyindex[TotalSurvey].surveying == false;
     }
     
     function addQuestion(string memory _question) public{
@@ -77,23 +84,24 @@ contract SurveyContract{
         Surveysheet storage _Surveysheet = surveyindex[TotalSurvey];
         require(_Surveysheet.surveying == true);
         uint _TotalAnswersheet = surveyindex[TotalSurvey].TotalAnswersheet;
-        require(_TotalQuestionsheet != 0 && _TotalQuestionsheet >= _TotalAnswersheet);
+        require(_TotalQuestionsheet != 0 && _TotalQuestionsheet > _TotalAnswersheet);
         Answersheet storage _Answersheet = surveyindex[TotalSurvey].AnswersheetIndex[_TotalAnswersheet];
         uint _TotalAnswer = _Answersheet.TotalAnswer;
         _Answersheet.AnswerIndex[_TotalAnswer].answer = _answer;
         surveyindex[TotalSurvey].AnswersheetIndex[_TotalAnswersheet].TotalAnswer++;
+        if(surveyindex[TotalSurvey].AnswersheetIndex[_TotalAnswersheet].TotalAnswer>= surveyindex[TotalSurvey].goal){
         surveyindex[TotalSurvey].TotalAnswersheet++;
+        }
     }
     
-     function addInfo(string memory _info) public{
-        Surveysheet storage _Surveysheet = surveyindex[TotalSurvey];
-        require(_Surveysheet.surveying == true);
-        uint _TotalAnswersheet = surveyindex[TotalSurvey].TotalAnswersheet;
-        Answersheet storage _Answersheet = surveyindex[TotalSurvey].AnswersheetIndex[_TotalAnswersheet];
-        uint _TotalInfo = _Answersheet.TotalInfo;
-        _Answersheet.InfoIndex[_TotalInfo].info = _info;
-        _TotalInfo++;
+     function Register(uint _age, string memory _sex, string memory _heigth, string memory _weigth, string memory _country) public{
+        InfoIndex[msg.sender].age =_age;
+        InfoIndex[msg.sender].sex =_sex;
+        InfoIndex[msg.sender].heigth =_heigth;
+        InfoIndex[msg.sender].weigth =_weigth;
+        InfoIndex[msg.sender].country =_country;
     }
+    
     
     ///////////////////////view///////////////////////////////////////////////
     
@@ -103,14 +111,12 @@ contract SurveyContract{
         _question = _Questionsheet.QuestionIndex[_questionID].question;
     }
     
-   function getAnswers(uint _surveyID, uint _answersheetID, uint _answerID) public view returns(string memory _answer, uint _TotalAnswer){
+   function getAnswers(uint _surveyID, uint _answersheetID, uint _answerID) public view returns( uint _TotalAnswer, string memory _answer, address _answerer){
         Answersheet storage _Answersheet = surveyindex[_surveyID].AnswersheetIndex[_answersheetID];
         _TotalAnswer = _Answersheet.TotalAnswer;
         _answer = _Answersheet.AnswerIndex[_answerID].answer;
+        _answerer = _Answersheet.answerer;
     }
     
-    function getInfo(uint _surveyID, uint _answersheetID, uint _infoID) public view returns(string memory _info){
-        Answersheet storage _Answersheet = surveyindex[_surveyID].AnswersheetIndex[_answersheetID];
-        _info = _Answersheet.InfoIndex[_infoID].info;
-    }
+  
 }
